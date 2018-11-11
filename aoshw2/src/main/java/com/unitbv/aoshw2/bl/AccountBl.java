3d61007f89f50de7c4891d2e085fe70109c91fc3
Petru <petruradulescu.v@gmail.com>
2018-11-11 20:56:17 +0200

    Pushed second homework

---
+++ aoshw2/src/main/java/com/unitbv/aoshw2/bl/AccountBl.java
@@ -0,0 +1,37 @@
+package com.unitbv.aoshw2.bl;
+
+import java.util.ArrayList;
+
+import com.unitbv.aoshw2.dal.AccountDal;
+import com.unitbv.aoshw2.dao.Person;
+
+public class AccountBl {
+	private static final String persistanceUnitName = "dbSource";
+	private AccountDal accountDal;
+	
+	public AccountBl() {
+		accountDal  = new AccountDal(persistanceUnitName);
+	}
+	
+	public void insert(Person person) {
+		if (person != null) {
+			accountDal.insert(person);
+		}
+	}
+
+	public void update(Person person) {
+		if (person != null) {
+			accountDal.update(person);
+		}
+	}
+
+	public ArrayList<Person> getAll() {
+			return accountDal.getAll();
+	}
+
+	public void delete(Person person) {
+		if (person != null) {
+			accountDal.delete(person);
+		}
+	}
+}
