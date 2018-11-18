9a3a378c10ce12245d2169682fc54cef656cee38
Petru <petruradulescu.v@gmail.com>
2018-11-18 21:03:46 +0200

    Fixed problems.Works now JPA implementation. Does not need additional SQL file to create the database!!!

---
+++ hw2/src/main/java/com/unitbv/hw2/Hw2Application.java
@@ -0,0 +1,12 @@
+package com.unitbv.hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2Application.class, args);
+	}
+}
